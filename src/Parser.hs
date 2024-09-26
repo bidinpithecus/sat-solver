e3e09f8e992c3cc42be6aeaccc4bab4fc58d08aa
bidinpithecus <vinibidin@gmail.com>
2024-09-26 13:17:29 -0300

    parser kinda works

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,10 +1,13 @@
 module Parser where
 
+import Data.List (intercalate)
+import Debug.Trace (trace)
 import System.Environment (getArgs)
 import Text.Parsec
   ( char,
     count,
     digit,
+    eof,
     many,
     many1,
     newline,
@@ -14,6 +17,7 @@ import Text.Parsec
     skipMany,
     spaces,
     string,
+    try,
   )
 import Text.Parsec.String (Parser)
 
@@ -22,7 +26,30 @@ type Literal = Int
 type Clause = [Literal]
 
 data CNF = CNF {numVars :: Int, numClauses :: Int, clauses :: [Clause]}
-  deriving (Show)
+
+toSubscript :: Int -> String
+toSubscript n = concatMap toSub (show n)
+  where
+    toSub '0' = "₀"
+    toSub '1' = "₁"
+    toSub '2' = "₂"
+    toSub '3' = "₃"
+    toSub '4' = "₄"
+    toSub '5' = "₅"
+    toSub '6' = "₆"
+    toSub '7' = "₇"
+    toSub '8' = "₈"
+    toSub '9' = "₉"
+    toSub _ = ""
+
+instance Show CNF where
+  show :: CNF -> String
+  show cnf = intercalate " ∧ " (map showClause (clauses cnf))
+    where
+      showClause clause = "(" ++ intercalate " ∨ " (map showLiteral clause) ++ ")"
+      showLiteral lit
+        | lit > 0 = "x" ++ toSubscript lit
+        | otherwise = "¬x" ++ toSubscript (negate lit)
 
 commentParser :: Parser ()
 commentParser = do
@@ -46,15 +73,17 @@ problemParser = do
 clauseParser :: Parser Clause
 clauseParser = do
   literals <- many1 $ do
-    lit <- read <$> many1 (oneOf "-0123456789")
+    lit <- read <$> many1 (oneOf "-123456789")
     spaces
     return lit
-  let clause = takeWhile (/= 0) literals
-  return clause
+  char '0'
+  spaces
+  return literals
 
 cnfParser :: Parser CNF
 cnfParser = do
   many commentParser
   (vars, clausesCount) <- problemParser
-  cls <- count clausesCount clauseParser
-  return $ CNF vars clausesCount cls
+  clauses <- count clausesCount clauseParser
+  eof
+  return $ CNF vars clausesCount clauses
